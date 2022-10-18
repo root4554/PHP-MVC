fe3be1d4e7af9edd9202c75ff8b7d6f447be8e08
root4554 <anass.kbir@hotmail.com>
2022-10-18 12:01:59 +0200

    add required to form

--- ejemplo_mvc/Views/V_formModificarUsuario.php
+++ ejemplo_mvc/Views/V_formModificarUsuario.php
@@ -11,9 +11,9 @@
 <body>
 
     <form action="C_modificarUsuario.php" method="post">
-        <input type="text" name="username" value="<?php echo $_GET['username']; ?>" readonly >
-        <input type="text" name="nombre" placeholder="nombre">
-        <input type="text" name="password" placeholder="password">
+        <input type="text" name="username" value="<?php echo $_GET['username']; ?>" readonly>
+        <input type="text" name="nombre" placeholder="nombre" required>
+        <input type="text" name="password" placeholder="password" required>
         <input type="submit" value="Modificar">
     </form>
 </body>
